3e4f8e8845a99e2156792130cd69d16fd65b65de
António Azevedo <83163489+antoniooazevedo@users.noreply.github.com>
2024-10-08 13:33:57 +0100

    Update auto_rr.sh

--- auto_rr.sh
+++ auto_rr.sh
@@ -3,29 +3,32 @@
 export DISPLAY=:0
 export XAUTHORITY=/home/antonio/.Xauthority
 
-DISPLAY=eDP
-CURR_RR=120
+CURR_RR=120.00
+
+xhost +local:
 
 xrandr -display :0.0 --newmode "1920x1200_60.00"  193.25  1920 2056 2256 2592  1200 1203 1209 1245 -hsync +vsync
 xrandr -display :0.0 --addmode eDP "1920x1200_60.00"
+
+getRefreshRate() {
+	xrandr | grep '*' | awk '{print $2}' | sed 's/[^0-9.]//g'
+}
 
 while true
 do
 	if [ $(cat /sys/class/power_supply/AC0/online) -eq 0 ]
 	then
-		if [ $CURR_RR -ne 60 ]
+		if [ "$(echo "$CURR_RR != 59.88" | bc)" -eq 1 ]
 		then
-			xrandr -display :0.0 --output $DISPLAY --mode "1920x1200_60.00" --primary
-			CURR_RR=60
+			xrandr -display :0.0 --output eDP --mode "1920x1200_60.00" --primary
 		fi
 	else
-		if [ $CURR_RR -ne 120 ]
+		if [ "$(echo "$CURR_RR != 120.00" | bc)" -eq 1 ]
 		then
-			xrandr -display :0.0 --output $DISPLAY --mode 1920x1200 -r 120 --primary
-			CURR_RR=120
+			xrandr -display :0.0 --output eDP --mode 1920x1200 -r 120 --primary
 		fi
 	fi
+	CURR_RR=$(getRefreshRate)
 	
 	sleep 2
 done
-	
